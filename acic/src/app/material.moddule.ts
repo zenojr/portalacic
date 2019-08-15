c4b64fc43ea9bfa4d4bb7a283c1d27491acdcdf6
Zeno França <zenofranca@MacBook-Air-de-Zeno.local>
2019-08-15 13:29:54 -0300

    update project

---
+++ acic/src/app/material.moddule.ts
@@ -0,0 +1,16 @@
+import {MatButtonModule,
+        MatCheckboxModule,
+        MatSidenavModule} from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [MatButtonModule,
+            MatCheckboxModule,
+            MatSidenavModule],
+
+  exports: [MatButtonModule,
+            MatCheckboxModule,
+            MatSidenavModule],
+})
+
+export class MaterialModule { }
