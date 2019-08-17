f923145bf33d4f22c14b27f39bcac6813bcf51b2
Zeno França <zenofranca@MacBook-Air-de-Zeno.local>
2019-08-17 15:07:48 -0300

    update project

--- acic/src/app/app.module.ts
+++ acic/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.moddule';
+import { BrowserModule            } from '@angular/platform-browser';
+import { NgModule                 } from '@angular/core';
+import { AppRoutingModule         } from './app-routing.module';
+import { AppComponent             } from './app.component';
+import { MenuComponent            } from './menu/menu.component';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
+import { MaterialModule           } from './material.module';
+import { TopoComponent } from './topo/topo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule,
