2d43a826b20ff930e1ce4279191fb0bfb38b6c31
Zeno França <zenofranca@MacBook-Air-de-Zeno.local>
2019-08-21 21:28:17 -0300

    update project

--- acic/src/app/app.module.ts
+++ acic/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent             } from './app.component';
 import { MenuComponent            } from './menu/menu.component';
 import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
 import { MaterialModule           } from './material.module';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { TopoComponent } from './topo/topo.component';
 
 
@@ -18,7 +20,8 @@ import { TopoComponent } from './topo/topo.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
