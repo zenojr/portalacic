cd08957dac893de4201a9da27ad5886ec400aaef
zenofjr <zfrancajunior@gmail.com>
2019-09-07 18:45:49 -0300

    update project

--- acic/src/app/banner-principal/banner-principal.component.ts
+++ acic/src/app/banner-principal/banner-principal.component.ts
@@ -20,8 +20,8 @@ export class BannerPrincipalComponent implements OnInit {
   selected = 0;
 
   tiles: Banner[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'One', cols: 2, rows: 2, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
   ];
