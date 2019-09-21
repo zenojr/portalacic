497a30071d51ac68213271b86bc35b2ae7900702
zenofjr <zfrancajunior@gmail.com>
2019-09-21 16:30:32 -0300

    update project

--- acic/src/app/noticias-destaque/noticias-destaque.component.ts
+++ acic/src/app/noticias-destaque/noticias-destaque.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-noticias-destaque',
   templateUrl: './noticias-destaque.component.html',
   styleUrls: ['./noticias-destaque.component.scss']
 })
 export class NoticiasDestaqueComponent implements OnInit {
+  
 
   constructor() { }
 
