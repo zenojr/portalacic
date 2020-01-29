a1b014f5b31d8071e75c6009cebaa6000ea4b64e
zenofjr <zfrancajunior@gmail.com>
2020-01-29 20:43:09 -0300

    update

--- acic/src/app/solucoes/solucoes.component.ts
+++ acic/src/app/solucoes/solucoes.component.ts
@@ -17,6 +17,7 @@ export class SolucoesComponent implements OnInit {
 
   textoSolution = '';
   auditText = 'Texto para o auditorio da acic';
+  certDigText = 'Texto da cert digitalllll';
 
   tabs = ['1º Notícia', 'Second', 'Third'];
   selected = 0;
@@ -42,8 +43,10 @@ export class SolucoesComponent implements OnInit {
     console.log(value);
     if( value === 'audit') {
       this.textoSolution = this.auditText;
+    } else if( value === 'certDig' ) {
+      this.textoSolution = this.certDigText;
     } else {
-      this.textoSolution = 'outro';
+      this.textoSolution = 'no data';
     }
 
     if ( this.solution === false ) {
@@ -59,7 +62,7 @@ export class SolucoesComponent implements OnInit {
       this.selected = 1;
     } else if ( this.selected === 1 ) {
       this.selected = 2;
-    } else if ( this.selected === 2  ) {
+    } else if ( this.selected === 2 ) {
       this.selected = 3;
     } else if ( this.selected === 3 ) {
       this.selected = 0;
